cec4a669a4a9f72eef173115f86e3604af48329a
Shaun Xu <jfarrio@gmail.com>
2013-10-29 13:30:17 +0800

    pong

--- server.js
+++ server.js
@@ -6,38 +6,43 @@ var intermediateProxyHost = config.intermediateProxyHost == '' ? null : config.i
 var intermediateProxyPort = config.intermediateProxyPort == '' ? null : config.intermediateProxyPort;
 
 http.createServer(function (req, res) {
-    console.log('client -> az-proxy: [' + req.method + '] ' + req.url);
-
-    var options = {
-        host: intermediateProxyHost || req.headers.host,
-        path: req.url,
-        method: req.method,
-        headers: req.headers
-    };
-    if (intermediateProxyPort) {
-        options.port = intermediateProxyPort;
+    if (req.url == '/') {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('PONG!');
     }
-    if (options.headers['Proxy-Connection']) {
-        options.headers['Connection'] = options.headers['Proxy-Connection'];
-        delete options.headers['Proxy-Connection'];
-    }
-    var innerRequest = http.request(options, function (innerResponse) {
-        console.log('remote -> az-proxy: [' + innerResponse.statusCode + '] ' + innerResponse.req.path);
+    else {
+        console.log('client -> az-proxy: [' + req.method + '] ' + req.url);
 
-        res.statusCode = innerResponse.statusCode;
-        for (var headerName in innerResponse.headers) {
-            res.setHeader(headerName, innerResponse.headers[headerName]);
+        var options = {
+            host: intermediateProxyHost || req.headers.host,
+            path: req.url,
+            method: req.method,
+            headers: req.headers
+        };
+        if (intermediateProxyPort) {
+            options.port = intermediateProxyPort;
         }
-        innerResponse.pipe(res);
-    });
+        if (options.headers['Proxy-Connection']) {
+            options.headers['Connection'] = options.headers['Proxy-Connection'];
+            delete options.headers['Proxy-Connection'];
+        }
+        var innerRequest = http.request(options, function (innerResponse) {
+            console.log('remote -> az-proxy: [' + innerResponse.statusCode + '] ' + innerResponse.req.path);
 
-    innerRequest.on('error', function (error) {
-        res.writeHead(500, {'Content-Type': 'text/plain'});
-        res.end(error.toString());
-    });
+            res.statusCode = innerResponse.statusCode;
+            for (var headerName in innerResponse.headers) {
+                res.setHeader(headerName, innerResponse.headers[headerName]);
+            }
+            innerResponse.pipe(res);
+        });
 
-    req.pipe(innerRequest);
+        innerRequest.on('error', function (error) {
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.end(error.toString());
+        });
 
+        req.pipe(innerRequest);
+    }
 }).listen(port);
 
 console.log('az-proxy is listening on port ' + port);
